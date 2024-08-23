7e23ea7bc80883b7119ff584cb92a55f3d76117b
Ayomided <adediji.d@yahoo.com>
2024-08-23 03:23:25 +0100

    feat: change entrpoint logic to overwrite with latest db

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,8 @@
 set -e
 
 # Check if blog.db exists and if its size is greater than 0
-if [ ! -f /data/blog.db ] || [ ! -s /data/blog.db ]; then
-  echo "Copying blog.db to /data directory..."
-  cp /usr/local/bin/blog.db /data/
-else
-  echo "blog.db already exists and is not empty."
-fi
+echo "Copying blog.db to /data directory..."
+cp /usr/local/bin/blog.db /data/
 
 # Run the main application
 exec "$@"
